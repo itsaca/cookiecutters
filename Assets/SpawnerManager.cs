b697991595110e21d7bd8d7e81ea4be9253a2e16
itsaca <jenna.aaltonen@laajasalonopisto.fi>
2023-01-12 14:07:58 +0200

    the android support

---
+++ Assets/SpawnerManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public float timer;
+    public float time = 2;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //spawning system here
+        while (timer < time) {
+            timer += Time.deltaTime;
+            if {timer > time}
+            timer -= time;
+        }
+
+    }
+}
