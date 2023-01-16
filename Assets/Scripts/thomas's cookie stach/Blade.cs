16b6c2bcc5f66f6137229b05397f41f882de7d60
TccL0 <tc.delarbre@gmail.com>
2023-01-16 12:21:16 +0200

    the cookie thing

---
+++ Assets/Scripts/thomas's cookie stach/Blade.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    public bool isCutting = false;
+    public float minSliceValue=.001f;
+    Rigidbody2D rb;
+    Camera cam;
+    Vector2 previousPosition;
+    public GameObject bladtrail;
+    GameObject currentbladtrail;
+    CircleCollider2D circleCollider2D;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        cam=Camera.main;
+        circleCollider2D=GetComponent<CircleCollider2D>();
+    }
+    void Update()
+    {
+        
+        
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if(isCutting)
+        {
+            UpdateBlade();
+        }
+
+    }
+    void UpdateBlade()
+        {
+            Vector2 newPosition=cam.ScreenToWorldPoint(Input.mousePosition);
+            rb.position = newPosition;
+            float velocity = (newPosition - previousPosition).magnitude*Time.deltaTime;
+            if (velocity > minSliceValue) 
+            {
+                circleCollider2D.enabled = true;
+            }else
+            { 
+                circleCollider2D.enabled = false;   
+            }
+            previousPosition= newPosition;
+
+        }
+    void StartCutting()
+    {
+        isCutting= true;
+        currentbladtrail = Instantiate(bladtrail, transform);
+        previousPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        circleCollider2D.enabled= false;
+    }
+
+    void StopCutting()
+    {
+        isCutting= false;
+        currentbladtrail.transform.SetParent(null);
+        Destroy(currentbladtrail);
+        circleCollider2D.enabled= false;    
+
+    }
+
+    
+}
---
+++ Assets/Scripts/thomas's cookie stach/cookiecutting.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cookiecutting : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "blade")
+        {
+            Debug.Log("cookie got cut!");
+            Destroy(gameObject);
+        }
+    }
+}
