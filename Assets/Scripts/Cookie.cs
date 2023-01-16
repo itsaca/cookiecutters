d0ef47e2dd282b4da4be8ddab568b19d83893ff3
itsaca <jenna.aaltonen@laajasalonopisto.fi>
2023-01-16 10:57:22 +0200

    lil code changes

--- Assets/Scripts/Cookie.cs
+++ Assets/Scripts/Cookie.cs
@@ -8,24 +8,14 @@ public class Cookie : MonoBehaviour
     public sweetspot ss;
     public RythmSync rs;
     float cookieLifespan = 4;
-    float cookieGoodness;
 
     void Start()
     {
-        cookieGoodness = -rs.variation;
+
     }
 
     void Update()
     {
-        //cookieGoodness += Time.deltaTime;
-        //if (cookieGoodness < -0.5 || cookieGoodness > 0.5) {
-        //    ss = sweetspot.poor;
-        //} else if (cookieGoodness < -0.3 || cookieGoodness > 0.3) {
-        //    ss = sweetspot.good;
-        //} else {
-        //    ss = sweetspot.perfect;
-        //}
-
         cookieLifespan -= Time.deltaTime;
         if (cookieLifespan < 0) {
             Destroy(gameObject);
