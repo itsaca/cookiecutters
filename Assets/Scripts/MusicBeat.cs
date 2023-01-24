50d1122f644c26a6781c5f48319edfe38ed38e51
itsaca <jenna.aaltonen@laajasalonopisto.fi>
2023-01-24 12:24:41 +0200

    beats

--- Assets/Scripts/MusicBeat.cs
+++ Assets/Scripts/MusicBeat.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class MusicBeat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Range(0, 5)] public int index;
+
 }
